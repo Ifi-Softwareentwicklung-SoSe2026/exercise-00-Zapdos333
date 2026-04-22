563c44fbd6ff972ffe2bb37b6f571e1fcfc7a29e
Ace009 <53367686+Zapdos333@users.noreply.github.com>
2026-04-22 12:42:55 +0200

    Create constructors, Validate and fix ToString for the weird special case used as example

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
     {
         Console.WriteLine("Mission startet...");
         var körper = Himmelskörper.Parse(args);
+        körper.ForceValidate();
         Console.WriteLine(körper.ToString());
     }
 }
@@ -19,6 +20,23 @@ class Himmelskörper
     public float? ScheinbareHelligkeit { get; set; }
     public float? Umlaufzeit { get; set; }
     public uint? ZentralkörperKatalogNummber { get; set; }
+    private Himmelskörper() {}
+    public Himmelskörper(string name, uint id, float erdJahre, uint zentralKörper)
+    {
+        Name = name;
+        Typ = 0;
+        KatalogNummer = id;
+        Umlaufzeit = erdJahre;
+        ZentralkörperKatalogNummber = zentralKörper;
+    }
+    public Himmelskörper(string name, uint id, bool isPlanet, Spektralklasse spektralklasse, float scheinbareHelligkeit)
+    {
+        Name = name;
+        Typ = isPlanet ? HimmelskörperTyp.Planet : HimmelskörperTyp.Mond;
+        KatalogNummer = id;
+        Spectral = spektralklasse;
+        ScheinbareHelligkeit = scheinbareHelligkeit;
+    }
     public static Himmelskörper Parse(string[] args)
     {
         return new Himmelskörper()
@@ -32,13 +50,46 @@ class Himmelskörper
             ZentralkörperKatalogNummber = uint.TryParse(args[6], out var zentralkörperKatalogNummber) ? zentralkörperKatalogNummber : null
         };
     }
+    public void ForceValidate()
+    {
+        var validationError = Validate();
+        if (validationError != null)
+            throw validationError;
+    }
+    public ArgumentException? Validate()
+    {
+        if (KatalogNummer >= 100000)
+            return new ArgumentException("Katalognummer ist ungültig.");
+        switch (Typ)
+        {
+            case HimmelskörperTyp.Stern:
+                if (!Spectral.HasValue || !ScheinbareHelligkeit.HasValue)
+                    return new ArgumentException("Fehlende Daten für einen Stern.");
+                if (ScheinbareHelligkeit.Value < 0.0)
+                    return new ArgumentException("Scheinbare Helligkeit ist ungültig.");
+                break;
+            case HimmelskörperTyp.Planet:
+            case HimmelskörperTyp.Mond:
+                if (!Umlaufzeit.HasValue || !ZentralkörperKatalogNummber.HasValue)
+                    return new ArgumentException("Fehlende Daten für einen Planeten oder Mond.");
+                if (Umlaufzeit.Value <= 0.0)
+                    return new ArgumentException("Umlaufzeit ist ungültig.");
+                if (ZentralkörperKatalogNummber.Value >= 100000)
+                    return new ArgumentException("Zentralkörper-Katalognummer ist ungültig.");
+                break;
+        }
+        return null;
+    }
+    #nullable disable // Static analysis doesn't know/care about Validate()
     public override string ToString()
     {
         string output = $"Himmelskörper: {Name}, Katalog-Nummer: {KatalogNummer}, Typ: {Typ}, ";
         if (Spectral.HasValue)
             output += $"Spektralklasse: {Spectral.Value}, Scheinbare Helligkeit: {ScheinbareHelligkeit.Value}";
-        if (Umlaufzeit.HasValue)
+        if (Umlaufzeit.HasValue) {
+            if (Spectral.HasValue) output += ", ";
             output += $"Umlaufzeit: {Umlaufzeit.Value} Erdjahre, Zentralkörper-Katalog-Nummer: {ZentralkörperKatalogNummber.Value}";
+        }
         return output;
     }
 }
