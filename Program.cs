cc60355619ebed996c66779be37748813c5c0210
Ace009 <53367686+Zapdos333@users.noreply.github.com>
2026-04-22 11:58:55 +0200

    Add parsing and ToString
    also removed public for warning about no namespace (CA1050)

--- Program.cs
+++ Program.cs
@@ -1,22 +1,16 @@
 ﻿
 class Program
 {
-    static void Main(String[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Mission startet...");
-        var eins = new Himmelskörper()
-        {
-            Name = "Erde",
-            KatalogNummer = 1,
-            Typ = HimmelskörperTyp.Planet,
-            Umlaufzeit = 1.0f,
-            ZentralkörperKatalogNummber = 0,
-        };
+        var körper = Himmelskörper.Parse(args);
+        Console.WriteLine(körper.ToString());
     }
 }
 
 
-public class Himmelskörper
+class Himmelskörper
 {
     public required string Name { get; set; }
     public required uint KatalogNummer { get; set; }
@@ -25,9 +19,31 @@ public class Himmelskörper
     public float? ScheinbareHelligkeit { get; set; }
     public float? Umlaufzeit { get; set; }
     public uint? ZentralkörperKatalogNummber { get; set; }
+    public static Himmelskörper Parse(string[] args)
+    {
+        return new Himmelskörper()
+        {
+            Name = args[0],
+            KatalogNummer = uint.Parse(args[1]),
+            Typ = Enum.Parse<HimmelskörperTyp>(args[2]),
+            Spectral = Enum.TryParse<Spektralklasse>(args[3], out var spektral) ? spektral : null,
+            ScheinbareHelligkeit = float.TryParse(args[4], out var scheinbareHelligkeit) ? scheinbareHelligkeit : null,
+            Umlaufzeit = float.TryParse(args[5], out var umlaufzeit) ? umlaufzeit : null,
+            ZentralkörperKatalogNummber = uint.TryParse(args[6], out var zentralkörperKatalogNummber) ? zentralkörperKatalogNummber : null
+        };
+    }
+    public override string ToString()
+    {
+        string output = $"Himmelskörper: {Name}, Katalog-Nummer: {KatalogNummer}, Typ: {Typ}, ";
+        if (Spectral.HasValue)
+            output += $"Spektralklasse: {Spectral.Value}, Scheinbare Helligkeit: {ScheinbareHelligkeit.Value}";
+        if (Umlaufzeit.HasValue)
+            output += $"Umlaufzeit: {Umlaufzeit.Value} Erdjahre, Zentralkörper-Katalog-Nummer: {ZentralkörperKatalogNummber.Value}";
+        return output;
+    }
 }
 
-public enum Spektralklasse
+enum Spektralklasse
 {
     O,
     B,
@@ -37,7 +53,7 @@ public enum Spektralklasse
     K,
     M
 }
-public enum HimmelskörperTyp
+enum HimmelskörperTyp
 {
     Stern,
     Planet,
